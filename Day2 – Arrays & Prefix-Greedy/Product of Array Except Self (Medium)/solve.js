935172f7012d54f174e86d17f4d5f4912d739ed1
Mohamed Salem <108944781+MoSalem149@users.noreply.github.com>
2026-02-22 13:58:09 +0200

    upload product of array except self problem

---
+++ Day2 – Arrays & Prefix-Greedy/Product of Array Except Self (Medium)/solve.js
@@ -0,0 +1,17 @@
+// O(n²) في nested loops
+var productExceptSelf = function (nums) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let product = 1;
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        product *= nums[j];
+      }
+    }
+    result.push(product);
+  }
+  return result;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4])); // [24,12,8,6]
+console.log(productExceptSelf([-1, 1, 0, -3, 3])); // [0,0,9,0,0]
