e983e031702798ee2e9dd15a54c70ccb6fd6302a
Mohamed Salem <108944781+MoSalem149@users.noreply.github.com>
2026-02-22 14:44:41 +0200

    upload maximum subarray problem

---
+++ Day2 – Arrays & Prefix-Greedy/Maximum Subarray (Medium)/solve.js
@@ -0,0 +1,16 @@
+// O(n²) في nested loops
+var maxSubArray = function (nums) {
+  let maxSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      maxSum = Math.max(maxSum, sum);
+    }
+  }
+  return maxSum;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+console.log(maxSubArray([1])); // 1
+console.log(maxSubArray([5, 4, -1, 7, 8])); // 23
